5b38169e1559bd9937051a907534281cb7a2b5d0
Adnan <eddy.khan2007@gmail.com>
2026-06-25 22:52:12 +0500

    code updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //this is from main
 console.log("hello world from main branch");
 //this is again from main branch
-console.log("hello again from main branch");
+// this is from dev1 
+console.log("hello from dev1");
 //this is from dev branch
 console.log("hello again from devs branch");
