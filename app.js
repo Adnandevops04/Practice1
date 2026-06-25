b46b308c53aac2556aab38c0145dd0a7337bc558
Adnan <eddy.khan2007@gmail.com>
2026-06-25 20:56:30 +0500

    dev code added

--- app.js
+++ app.js
@@ -2,3 +2,5 @@
 console.log("hello world from main branch");
 //this is again from main branch
 console.log("hello again from main branch");
+//this is from dev branch
+console.log("hello again from devs branch");
