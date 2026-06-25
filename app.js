0412696ca1fae920417a1d31a260dacdbd194cad
Adnandevops04 <eddy.khan2007@gmail.com>
2026-06-25 22:43:40 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //this is from main
 console.log("hello world from main branch");
 //this is again from main branch
-console.log("hello again from main branch");
+// this is from dev-2 branch
+console.log("hello from dev2");
 //this is from dev branch
 console.log("hello again from devs branch");
